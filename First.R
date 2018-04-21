92cc4de3b88221eeeaf3355e59dce3d9f2136909
chiragdaga777 <38504610+chiragdaga777@users.noreply.github.com>
2018-04-21 23:54:14 +0530

    Add files via upload

---
+++ First.R
@@ -0,0 +1,9 @@
+plot (women)
+
+#Chirags-Air:~ chiragdaga$ cd '/Users/chiragdaga/Desktop/rWork/rProjects/analytics'
+#Chirags-Air:analytics chiragdaga$ 
+#git config --global user.email "chiragdaga777@gmail.com"
+#git config --global user.name "Chirag Daga"
+
+#Chirags-Air:~ chiragdaga$ cd '/Users/chiragdaga/Desktop/rWork/rProjects/analytics'
+#Chirags-Air:analytics chiragdaga$ 
