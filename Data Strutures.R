421086a5cbbe2c6d4c8c2ed31429fd38372bc55b
Chirag Daga <chiragdaga777@gmail.com>
2018-04-28 14:49:22 +0530

    abcd

--- Data Strutures.R
+++ Data Strutures.R
@@ -181,9 +181,13 @@ gradesF2
 (a1 = array (1:24, dim=c(4,3,2)))
 (df1 = data.frame(rollno=c(1:5), sname=c(paste('S',1:5, sep='-'))))
 class(v1); class(m1); class(a1); class(df1)
-L1 = list(v1, m1, a1, df1)
+
+L1 = list(v1, m1, a1, df1) #List
 L1
-L1[3]; L1[2]
+L1[1]; L1[2]; L1[3]; L1[4]
+(v2 = c('a','b'))
+L2 = list(v2, L1)
+L2
 
 
 
